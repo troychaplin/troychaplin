1aaf218099472dff0cb05e76a1ecc0d7b6a36560
Troy Chaplin <troy.chaplin@gmail.com>
2026-02-15 14:46:57 -0500

    wceh styles

---
+++ wceh/patterns/camp-stats.php
@@ -0,0 +1,51 @@
+<?php
+/**
+ * Title: Camp Stats
+ * Description: A section highlighting key statistics about the camp.
+ * Slug: wceh2026/camp-stats
+ * Categories: WCEH2026
+ */
+?>
+<!-- wp:group {"metadata":{"name":"Camp Stats"},"align":"full","className":"wide-group wide-group\u002d\u002dtransparent","style":{"spacing":{"padding":{"top":"var:preset|spacing|50","bottom":"var:preset|spacing|50"}}},"layout":{"type":"constrained"}} -->
+<div class="wp-block-group alignfull wide-group wide-group--transparent" id="stats" style="padding-top:var(--wp--preset--spacing--50);padding-bottom:var(--wp--preset--spacing--50)"><!-- wp:group {"metadata":{"name":"Card Row"},"align":"wide","className":"card-grid card-grid\u002d\u002dfour","style":{"spacing":{"blockGap":"var:preset|spacing|30"}},"layout":{"type":"flex","flexWrap":"nowrap","verticalAlignment":"center","justifyContent":"center"}} -->
+<div class="wp-block-group alignwide card-grid card-grid--four"><!-- wp:group {"metadata":{"name":"Stat Card"},"className":"card-grid__item","style":{"spacing":{"blockGap":"0"},"layout":{"selfStretch":"fill","flexSize":null}},"layout":{"type":"constrained"}} -->
+<div class="wp-block-group card-grid__item"><!-- wp:paragraph {"align":"center","style":{"typography":{"fontStyle":"normal","fontWeight":"800"},"elements":{"link":{"color":{"text":"var:preset|color|custom-highlight"}}}},"textColor":"custom-highlight","fontSize":"xx-large"} -->
+<p class="has-text-align-center has-custom-highlight-color has-text-color has-link-color has-xx-large-font-size" style="font-style:normal;font-weight:800">262</p>
+<!-- /wp:paragraph -->
+
+<!-- wp:paragraph {"align":"center","fontSize":"small"} -->
+<p class="has-text-align-center has-small-font-size">Days until WordCamp</p>
+<!-- /wp:paragraph --></div>
+<!-- /wp:group -->
+
+<!-- wp:group {"metadata":{"name":"Stat Card"},"className":"card-grid__item","style":{"spacing":{"blockGap":"0"},"layout":{"selfStretch":"fill","flexSize":null}},"layout":{"type":"constrained"}} -->
+<div class="wp-block-group card-grid__item"><!-- wp:paragraph {"align":"center","style":{"typography":{"fontStyle":"normal","fontWeight":"800"},"elements":{"link":{"color":{"text":"var:preset|color|custom-highlight"}}}},"textColor":"custom-highlight","fontSize":"xx-large"} -->
+<p class="has-text-align-center has-custom-highlight-color has-text-color has-link-color has-xx-large-font-size" style="font-style:normal;font-weight:800">20+</p>
+<!-- /wp:paragraph -->
+
+<!-- wp:paragraph {"align":"center","fontSize":"small"} -->
+<p class="has-text-align-center has-small-font-size">Sessions &amp; Workshops</p>
+<!-- /wp:paragraph --></div>
+<!-- /wp:group -->
+
+<!-- wp:group {"metadata":{"name":"Stat Card"},"className":"card-grid__item","style":{"spacing":{"blockGap":"0"},"layout":{"selfStretch":"fill","flexSize":null}},"layout":{"type":"constrained"}} -->
+<div class="wp-block-group card-grid__item"><!-- wp:paragraph {"align":"center","style":{"typography":{"fontStyle":"normal","fontWeight":"800"},"elements":{"link":{"color":{"text":"var:preset|color|custom-highlight"}}}},"textColor":"custom-highlight","fontSize":"xx-large"} -->
+<p class="has-text-align-center has-custom-highlight-color has-text-color has-link-color has-xx-large-font-size" style="font-style:normal;font-weight:800">24+</p>
+<!-- /wp:paragraph -->
+
+<!-- wp:paragraph {"align":"center","fontSize":"small"} -->
+<p class="has-text-align-center has-small-font-size">Speakers</p>
+<!-- /wp:paragraph --></div>
+<!-- /wp:group -->
+
+<!-- wp:group {"metadata":{"name":"Stat Card"},"className":"card-grid__item","style":{"spacing":{"blockGap":"0"},"layout":{"selfStretch":"fill","flexSize":null}},"layout":{"type":"constrained"}} -->
+<div class="wp-block-group card-grid__item"><!-- wp:paragraph {"align":"center","style":{"typography":{"fontStyle":"normal","fontWeight":"800"},"elements":{"link":{"color":{"text":"var:preset|color|custom-highlight"}}}},"textColor":"custom-highlight","fontSize":"xx-large"} -->
+<p class="has-text-align-center has-custom-highlight-color has-text-color has-link-color has-xx-large-font-size" style="font-style:normal;font-weight:800">2</p>
+<!-- /wp:paragraph -->
+
+<!-- wp:paragraph {"align":"center","fontSize":"small"} -->
+<p class="has-text-align-center has-small-font-size">Days of Learning</p>
+<!-- /wp:paragraph --></div>
+<!-- /wp:group --></div>
+<!-- /wp:group --></div>
+<!-- /wp:group -->
